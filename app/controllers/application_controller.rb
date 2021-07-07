8cc3d09de709cf873fe4946a9bb3d794f2633877
yutaro313 <yuutarou55@icloud.com>
2021-07-07 10:43:10 +0000

    [add]devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::
-   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # ログイン後のページ遷移
   def after_sign_in_path_for(resource)
--- app/controllers/sale/customers_controller.rb
+++ app/controllers/sale/customers_controller.rb
@@ -1,4 +1,5 @@
 class Sale::CustomersController < ApplicationController
+  
 
   def index
   end
