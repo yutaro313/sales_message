f5be279d2ddd6266254d5eb498399c41d441c5e9
yuttaro313 <yuutarou55@icloud.com>
2021-07-29 06:51:14 +0000

    [fix]profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 class Profile < ApplicationRecord
   belongs_to :sale
 
-  attachment :profile_image, content_type: ["profile_image/jpeg", "profile_image/png"]
+  attachment :profile_image
 
   # バリデーション
   validates :name, :birthplace, :hobby, :recent_event, :phone_number, :a_word, presence: true
